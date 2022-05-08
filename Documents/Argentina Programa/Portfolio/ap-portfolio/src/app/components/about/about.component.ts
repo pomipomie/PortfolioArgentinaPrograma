6cfdeba952b7ed9f24cd7fedb10088e0935fe4b3
Xio <furikixio@gmail.com>
2022-05-08 03:28:02 -0300

    EDITION IS FINALLY WORKING

--- Documents/Argentina Programa/Portfolio/ap-portfolio/src/app/components/about/about.component.ts
+++ Documents/Argentina Programa/Portfolio/ap-portfolio/src/app/components/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { AutenticationService } from 'src/app/services/autentication.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
@@ -19,9 +19,9 @@ export class AboutComponent implements OnInit {
   constructor(private datosPortfolio:PortfolioService, private autenticationService:AutenticationService, private formBuilder:FormBuilder) {
     this.form1 = formBuilder.group(
       {
-        picUrl: [''],
-        firstName:[''],
-        lastName:['']
+        picUrl: ['', [Validators.required]],
+        firstName:['', [Validators.required]],
+        lastName:['', [Validators.required]]
       }
     );
   }
@@ -38,52 +38,24 @@ export class AboutComponent implements OnInit {
     }
   }
 
-  get picUrl() {
-    return this.form1.get('picUrl');
-  }
-
-  get firstName() {
-    return this.form1.get('firstName');
-  }
-
-  get lastName() {
-    return this.form1.get('lastName');
-  }
-
   onSave() {
     //alert(JSON.stringify(this.form1.value)); //para pruebas
     if (this.form1.valid) {
-     // console.log("form submitted!"); //para pruebas
-      // let firstName = this.form1.get('firstName')?.value;
-      // let lastName = this.form1.get('lastName')?.value;
-      // let picUrl = this.form1.get('picUrl')?.value;
-      // console.log(firstName+" "+lastName+" "+picUrl);//para pruebas
-      // let data:any [] = [firstName, lastName, picUrl];
-      // console.log("data: "+data);
-      let data = JSON.stringify(this.form1.value);
-      console.log("line 64: "+data); //TEST
-      console.log(this.form1.value);
       let d = this.form1.value;
-      console.log(d);
+      console.log(d.firstName);
+      d.aboutText = this.myPortfolio.aboutText;
+      d.birthDate = this.myPortfolio.birthDate;
+      d.location = this.myPortfolio.location;
+      d.phone = this.myPortfolio.phone;
+      d.email = this.myPortfolio.email;
+      d.password = this.myPortfolio.password;
+      d.id = this.autenticationService.getUserLogged();
+      //console.log(d); //TEST
 
-    // this.autenticationService.editUser(this.autenticationService.getUserLogged()).subscribe(
-    //   data => {
-    //     console.log("line 66: "+data); //TEST
-    //     this.myPortfolio = data;
-    //   }
-    // )
-    let id = this.autenticationService.getUserLogged();
-    this.datosPortfolio.editUser(id).subscribe(
-      // data => {
-      //   console.log("line 66: "+data); //TEST
-      //   this.myPortfolio = this.form1.value;
-      // }
+    this.datosPortfolio.editUser(d).subscribe(
       d => {
-        this.myPortfolio = d;
-      }
-    )
-
-          console.log("id: "+this.autenticationService.getUserLogged()); //para pruebas
+        this.user = d;
+      })
           }
     else{
       alert("Formulario inválido");
